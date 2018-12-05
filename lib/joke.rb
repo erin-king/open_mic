a718e6223eee7dc012d06345a4e1be1920be441b
Erin King <eohone@gmail.com>
2018-12-05 09:36:10 -0700

    added test_if_punchline_returns_a_value

--- lib/joke.rb
+++ lib/joke.rb
@@ -2,11 +2,13 @@ require 'pry'
 
 class Joke
   attr_reader :id,
-              :setup
+              :setup,
+              :punchline
 
   def initialize(id, setup, punchline)
     @id = id
     @setup = setup
+    @punchline = punchline
   end
 
 
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -21,5 +21,10 @@ class JokeTest < Minitest::Test
 
     assert_equal "Why did the strawberry cross the road?", joke.setup
   end
+
+  def test_if_punchline_returns_a_value
+    joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+
+    assert_equal "Because his mother was in a jam.", joke.punchline
+  end
 end
-# , "Because his mother was in a jam.")
